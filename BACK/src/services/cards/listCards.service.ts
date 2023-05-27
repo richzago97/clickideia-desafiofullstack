cc0936377c63849a26902bf4c27b09818823790d
richzago97 <rich.czago@gmail.com>
2023-05-27 10:13:47 -0300

    feat: adicionado novas tipagens

--- BACK/src/services/cards/listCards.service.ts
+++ BACK/src/services/cards/listCards.service.ts
@@ -1,10 +1,12 @@
+import { Repository, TreeRepository } from "typeorm";
 import { AppDataSource } from "../../data-source";
 import { Card } from "../../entities/cards.entity";
 
 const listCardsService = async () => {
-    const cardsRepository = AppDataSource.getTreeRepository(Card);
+    const cardsRepository: TreeRepository<Card> =
+        AppDataSource.getTreeRepository(Card);
 
-    const cards = await cardsRepository.find();
+    const cards: Card[] = await cardsRepository.find();
 
     return cards;
 };
