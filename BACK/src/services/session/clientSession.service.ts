292685c41590706325cbe4e9b23e244c072b7e3e
richzago97 <rich.czago@gmail.com>
2023-05-27 09:30:34 -0300

    feat: criado a logica do login

--- BACK/src/services/session/clientSession.service.ts
+++ BACK/src/services/session/clientSession.service.ts
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 const clientSessionService = (): string => {
     const login = process.env.login;
     const senha = process.env.senha;
-    const token = jwt.sign({ login, senha }, process.env.SECRET_KEY as string);
+    const expiresIn = process.env.expiresIn;
+    const token = jwt.sign({ login, senha }, process.env.SECRET_KEY as string, {
+        expiresIn,
+    });
     return token;
 };
 
