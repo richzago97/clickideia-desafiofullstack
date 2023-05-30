320aecde159098868b1f2d2efc6e6b3c70aed7fa
richzago97 <rich.czago@gmail.com>
2023-05-30 09:27:32 -0300

    echo: ajustado a indentação

--- FRONT/src/Components/Cards/Create/index.tsx
+++ FRONT/src/Components/Cards/Create/index.tsx
@@ -7,59 +7,55 @@ import { CardsContext } from "../../../Contexts/cardContext";
 import { CardProps } from "../../../Interfaces/cards.interface";
 
 type CardFormValues = {
-    titulo: string;
-    conteudo: string;
+   titulo: string;
+   conteudo: string;
 };
 
 export const CreateCard = () => {
-    const { addCard, setCards } = useContext<any>(CardsContext);
-    const { register, handleSubmit, reset } = useForm<CardFormValues>();
+   const { createCard, setCards } = useContext<any>(CardsContext);
+   const { register, handleSubmit, reset } = useForm<CardFormValues>();
 
-    const onSubmit = async (data: CardFormValues) => {
-        const { titulo, conteudo } = data;
+   const onSubmit = async (data: CardFormValues) => {
+      const { titulo, conteudo } = data;
 
-        try {
-            // Criar um objeto CardProps com as propriedades necessárias
-            const card: CardProps = {
-                id: "", // O valor do id será gerado pelo backend
-                titulo: titulo,
-                conteudo: conteudo,
-                lista: "To Do", // Definir a lista como "To Do"
-            };
+      try {
+         const card: CardProps = {
+            id: "",
+            titulo: titulo,
+            conteudo: conteudo,
+            lista: "To Do",
+         };
 
-            // Chamar a função addCard do contexto passando o objeto card
-            await addCard(card);
 
-            // Atualizar os cards no contexto com o novo card adicionado
-            // setCards((cards: any) => [...cards, card]);
-            setCards([card]);
+         await createCard(card);
 
-            // Limpar o formulário após adicionar o card
-            reset();
-        } catch (error) {
-            console.error("Erro ao criar o card:", error);
-        }
-    };
+         setCards([card]);
 
-    return (
-        <Container>
-            <h2>Novo</h2>
-            <form className="formCard" onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    className="inputCreate"
-                    placeholder="Título"
-                    {...register("titulo", { required: true })}
-                />
-                <textarea
-                    className="textArea"
-                    placeholder="Conteúdo"
-                    {...register("conteudo", { required: true })}
-                />
+         reset();
+      } catch (error) {
+         console.error("Erro ao criar o card:", error);
+      }
+   };
 
-                <Button buttonType="submit">
-                    <IoIosAddCircleOutline />
-                </Button>
-            </form>
-        </Container>
-    );
+   return (
+      <Container>
+         <h2>Novo</h2>
+         <form className="formCard" onSubmit={handleSubmit(onSubmit)}>
+            <input
+               className="inputCreate"
+               placeholder="Título"
+               {...register("titulo", { required: true })}
+            />
+            <textarea
+               className="textArea"
+               placeholder="Conteúdo"
+               {...register("conteudo", { required: true })}
+            />
+
+            <Button buttonType="submit">
+               <IoIosAddCircleOutline />
+            </Button>
+         </form>
+      </Container>
+   );
 };
