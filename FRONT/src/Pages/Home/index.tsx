2695ff8507236797eae4eff5326d186af11ea385
richzago97 <rich.czago@gmail.com>
2023-05-30 09:30:28 -0300

    echo: ajustado a indentação

--- FRONT/src/Pages/Home/index.tsx
+++ FRONT/src/Pages/Home/index.tsx
@@ -1,40 +1,5 @@
-import { useEffect, useState } from "react";
 import { Container } from "./style";
-import { CardProps } from "../../Interfaces/cards.interface";
-import axios from "axios";
-
-const [cards, setCards] = useState<CardProps[]>([]);
-const [newCardTitle, setNewCardTitle] = useState("");
-const [newCardDescription, setNewCardDescription] = useState("");
-
-useEffect(() => {
-    fetchCards();
-}, []);
-
-const fetchCards = async () => {
-    try {
-        const response = await axios.get("/cards");
-        setCards(response.data);
-    } catch (error) {
-        console.log("Error:", error);
-    }
-};
-
-const handleAddCard = async () => {
-    try {
-        const response = await axios.post("/cards", {
-            title: newCardTitle,
-            description: newCardDescription,
-            list: "ToDo",
-        });
-        setCards([...cards, response.data]);
-        setNewCardTitle("");
-        setNewCardDescription("");
-    } catch (error) {
-        console.log("Error:", error);
-    }
-};
 
 export const Home = () => {
-    return <Container></Container>;
+   return <Container></Container>;
 };
