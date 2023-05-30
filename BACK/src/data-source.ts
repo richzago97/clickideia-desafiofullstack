2589cf9b2f6715731555e59fdbc33d23f3c0086b
richzago97 <rich.czago@gmail.com>
2023-05-30 09:33:44 -0300

    feat: nova configuração

--- BACK/src/data-source.ts
+++ BACK/src/data-source.ts
@@ -1,27 +1,15 @@
+import { DataSource } from "typeorm";
 import "dotenv/config";
-import path from "path";
-import "reflect-metadata";
-import { DataSource, DataSourceOptions } from "typeorm";
 
-const dataSourceConfig = (): DataSourceOptions => {
-    const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
-    const migrationPath: string = path.join(
-        __dirname,
-        "./migrations/**.{ts,js}"
-    );
-
-    const dbUrl: string | undefined = process.env.DATABASE_URL;
-
-    if (!dbUrl) throw new Error("Missing env var: 'DATABASE_URL'");
-
-    return {
-        type: "postgres",
-        url: dbUrl,
-        synchronize: false,
-        logging: true,
-        entities: [entitiesPath],
-        migrations: [migrationPath],
-    };
-};
-
-export const AppDataSource = new DataSource(dataSourceConfig());
+export const AppDataSource = new DataSource({
+   type: "postgres",
+   host: process.env.POSTGRES_HOST!,
+   port: Number(process.env.PGPORT!),
+   username: process.env.POSTGRES_USER!,
+   password: process.env.POSTGRES_PASSWORD!,
+   database: process.env.POSTGRES_DB!,
+   logging: true,
+   synchronize: false,
+   entities: ["src/entities/*.ts"],
+   migrations: ["src/migrations/*.ts"],
+});
